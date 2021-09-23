94ea3e7a131c01cbb6ed19dfd7217f5ac96dbaf8
harrymichael19 <85347224+harrymichael19@users.noreply.github.com>
2021-09-23 21:16:17 +0100

    Update Temperature converter.py

--- Temperature converter.py
+++ Temperature converter.py
@@ -14,10 +14,10 @@ def calculate():
     entry.delete(0,END) #remove value after entry
     
 root = Tk()
-message_label = Label(text = "Enter a temperature", font = ("Verdana", 16))
-output_label = Label(font = ("Verdana", 16))
-entry = Entry(font = ("Verdana", 16), width = 4)
-calc_button = Button(text = "Ok", font = ("Verdana" , 16), command = calculate)
+message_label = Label(text = "Enter a temperature", font = ("Helvetica", 16))
+output_label = Label(font = ("Helvetica", 16))
+entry = Entry(font = ("Helvetica", 16), width = 4)
+calc_button = Button(text = "Ok", font = ("Helvetica" , 16), command = calculate)
 
 message_label.grid(row = 0, column = 0)
 entry.grid(row = 0, column = 1)
